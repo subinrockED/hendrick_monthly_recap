c3b1333bf0c9785613acaca06981ebebc8c99fda
subinrockED <subin.lebow@rocked.us>
2025-08-31 16:26:13 -0400

    Delete gui_test.py

--- gui_test.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("My Python Script")
-input_text = st.text_input("Enter some text")
-if st.button("Run"):
-    # Your script code here
-    result = input_text.upper()  # Example transformation
-    st.write("Result:", result)
